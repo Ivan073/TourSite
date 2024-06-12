5c9a60bac2ce3567cc98d6876e2e9e654bd9bcc3
Ivan <vanya_zhechko@tut.by>
2024-06-12 13:16:57 +0300

    Added exception handlers for all controllers

--- src/main/java/samsolutions/site/tour/controllers/TourController.java
+++ src/main/java/samsolutions/site/tour/controllers/TourController.java
@@ -27,49 +27,69 @@ public class TourController {
 
     @PostMapping
     public ResponseEntity<TourDTO> postTours(@RequestBody TourDTO tourdto) {
-        Tour entity = TourConverter.convertToEntity(tourdto);
-        tourService.createTour(entity);
-        return new ResponseEntity<>(TourConverter.convertToDTO(entity), HttpStatus.CREATED);
+        try {
+            Tour entity = TourConverter.convertToEntity(tourdto);
+            tourService.createTour(entity);
+            return new ResponseEntity<>(TourConverter.convertToDTO(entity), HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+        }
     }
 
     @GetMapping
-    public List<Tour> getTours() {
-        return tourService.getTours();
+    public ResponseEntity<List<Tour>> getTours() {
+        try {
+            return new ResponseEntity<>(tourService.getTours(), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+        }
+
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TourDTO> getTourById(@PathVariable long id) {
-        Optional<Tour> entity = tourService.getTourById(id);
+        try {
+            Optional<Tour> entity = tourService.getTourById(id);
 
-        if (entity.isPresent()) {
-            return ResponseEntity.ok(TourConverter.convertToDTO(entity.get()));
-        } else {
-            return ResponseEntity.notFound().build();
+            if (entity.isPresent()) {
+                return ResponseEntity.ok(TourConverter.convertToDTO(entity.get()));
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<TourDTO> deleteTourById(@PathVariable long id) {
-        Optional<Tour> entity = tourService.getTourById(id);
+        try {
+            Optional<Tour> entity = tourService.getTourById(id);
 
-        if (entity.isPresent()) {
-            tourService.deleteTourById(id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
+            if (entity.isPresent()) {
+                tourService.deleteTourById(id);
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
         }
     }
 
     @PutMapping
     public ResponseEntity<TourDTO> updateTour(@RequestBody TourDTO tourdto) {
-        Tour entity = TourConverter.convertToEntity(tourdto);
-        int id = entity.getId();
-        if (tourService.getTourById(id).isPresent()) {
-            tourService.updateTour(entity);
-            return new ResponseEntity<>(TourConverter.convertToDTO(entity), HttpStatus.CREATED);
-        } else {
-            return ResponseEntity.notFound().build();
+        try{
+            Tour entity = TourConverter.convertToEntity(tourdto);
+            int id = entity.getId();
+            if (tourService.getTourById(id).isPresent()) {
+                tourService.updateTour(entity);
+                return new ResponseEntity<>(TourConverter.convertToDTO(entity), HttpStatus.CREATED);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
         }
-
     }
 }
