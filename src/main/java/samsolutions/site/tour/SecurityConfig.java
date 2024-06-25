b1feefbfa2b260cf017f50bcc5a8540d57c0d98c
Ivan <vanya_zhechko@tut.by>
2024-06-25 17:38:27 +0300

    Authentication connected to client

--- src/main/java/samsolutions/site/tour/SecurityConfig.java
+++ src/main/java/samsolutions/site/tour/SecurityConfig.java
@@ -2,30 +2,73 @@ package samsolutions.site.tour;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeHttpRequests) ->
+        http
+               .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.setAllowCredentials(true);
+                    return corsConfiguration;
+                })).
+                authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
-                                .requestMatchers("/tours").hasRole("USER")
-                                .requestMatchers("/tours/images/**").permitAll()
-                                .anyRequest().permitAll()
+                             //   .requestMatchers("/tours").hasRole("USER")
+                             //   .requestMatchers("/tours/14").hasRole("USER")
+                             //   .requestMatchers("/tours/images/**").permitAll()
+                             //   .anyRequest().permitAll()
+                                .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .loginPage("http://localhost:8081/login")
-                        .defaultSuccessUrl("http://google.com")
-                        .failureUrl("http://localhost:8081/login?error")
-                        .loginProcessingUrl("http://localhost:8081/login")
+                .formLogin(login -> login.defaultSuccessUrl("http://localhost:3000/"))
+                .csrf((csrf) -> csrf
+                        .disable()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-             //   .logout(logout -> logout.permitAll());
+                .logout(logout -> logout.permitAll());
         return http.build();
+    }
+
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user =
+                User.withUsername("user")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
